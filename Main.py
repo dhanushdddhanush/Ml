8007dc4514156ca1b4404da497372c5f5be0568d
Dhanush <98016619+dhanushdddhanush@users.noreply.github.com>
2023-12-29 14:48:20 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+
+st.set_page_config(layout ="wide")
+st.title("Student Management System")
+st.sidebar.title("Fee Management System")
+
+st.sidebar.subheader("Add Student")
+rollnumber=st.sidebar.number_input("Roll Number")
+name=st.sidebar.text_input("Name")
+fees=st.sidebar.number_input("Fees")
+add=st.sidebar.button("Add")
